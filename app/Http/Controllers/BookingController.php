cc4a618963ca3d6e7e97bbdff022dcc963688769
fakhrizaki27 <fakhrizaki27@gmail.com>
2024-12-13 02:48:42 +0700

    add passenger-details and save data passengerDetails

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePassengerDetailRequest;
 use App\Interfaces\FlightRepositoryInterface;
 use App\Interfaces\TransactionRepositoryInterface;
 use Illuminate\Http\Request;
@@ -35,8 +36,33 @@ class BookingController extends Controller
     }
 
     public function confirmSeat(Request $request, $flightNumber){
-        dd($request->all());
+
         $this->transactionRepository->saveTransactionDataToSession($request->all());
+        return redirect()->route('booking.passengerDetails', ['flightNumber' => $flightNumber]);
+    }
+
+    public function passengerDetails(Request $request, $flightNumber)
+    {
+        $transaction = $this->transactionRepository->getTransactionDataFromSession();
+        $flight = $this->flightRepository->getFlightByFlightNumber($flightNumber);
+        $tier = $flight->classes->find($transaction['flight_class_id']);
+
+        return view('pages.booking.passenger-details', compact('transaction', 'flight', 'tier'));
+    }
+
+    public function savePassengerDetails(StorePassengerDetailRequest $request, $flightNumber)
+    {
+        $this->transactionRepository->saveTransactionDataToSession($request->all());
+        return redirect()->route('booking.checkout', ['flightNumber' => $flightNumber]);
+    }
+
+    public function checkout($flightNumber)
+    {
+        $transaction = $this->transactionRepository->getTransactionDataFromSession();
+        $flight = $this->flightRepository->getFlightByFlightNumber($flightNumber);
+        $tier = $flight->classes->find($transaction['flight_class_id']);
+
+        return view('pages.booking.passenger-details', compact('transaction', 'flight', 'tier'));
     }
 
     public function checkBooking()
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,13 @@ Route::get('flights', [FlightController::class, 'index'])->name('flight.index');
 Route::get('flight/{flightNumber}/choose-tier', [FlightController::class, 'show'])->name('flight.show');
 
 Route::get('flight/booking/{flightNumber}', [BookingController::class, 'booking'])->name('booking');
+
 Route::get('flight/booking/{flightNumber}/choose-seat', [BookingController::class, 'chooseSeat'])->name('booking.chooseSeat');
 Route::post('flight/booking/{flightNumber}/confirm-seat', [BookingController::class, 'confirmSeat'])->name('booking.confirmSeat');
+
+Route::get('flight/booking/{flightNumber}/passenger-details', [BookingController::class, 'passengerDetails'])->name('booking.passengerDetails');
+Route::post('flight/booking/{flightNumber}/save-passenger-details', [BookingController::class, 'savePassengerDetails'])->name('booking.savePassengerDetails');
+
+Route::get('flight/booking/{flightNumber}/checkout', [BookingController::class, 'checkout'])->name('booking.checkout');
 
 Route::get('check-booking', [BookingController::class, 'checkBooking'])->name('booking.check');
